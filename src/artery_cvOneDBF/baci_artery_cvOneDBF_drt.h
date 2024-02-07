352a30130f592c9bc958fc9829be2e4cbacbe23b
Martin Frank <martin.frank@unibw.de>
2024-02-07 18:36:41 +0100

    Create new Driver function for problem

---
+++ src/artery_cvOneDBF/baci_artery_cvOneDBF_drt.h
@@ -0,0 +1,23 @@
+/*----------------------------------------------------------------------*/
+/*! \file
+
+\brief  Main control routine for partitioned coupled 1D artery and 3d fluid simulations
+
+\level 3
+
+*/
+/*----------------------------------------------------------------------*/
+
+#ifndef BACI_ARTERY_CVONEDBF_DRT_H
+#define BACI_ARTERY_CVONEDBF_DRT_H
+
+#include "baci_config.H"
+
+
+BACI_NAMESPACE_OPEN
+
+void artery_cvOneDBF_drt();
+
+BACI_NAMESPACE_CLOSE
+
+#endif
