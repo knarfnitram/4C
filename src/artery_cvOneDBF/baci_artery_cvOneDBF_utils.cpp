033c872cdf2f7f0e8e8800c799a52168398bfe67
Martin Frank <martin.frank@unibw.de>
2024-02-08 17:30:09 +0100

    Create single execution utils function

---
+++ src/artery_cvOneDBF/baci_artery_cvOneDBF_utils.cpp
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------*/
+/*! \file
+
+\brief  Utils functions partitioned coupled 1D artery and 3d fluid simulations
+
+\level 3
+
+*/
+/*----------------------------------------------------------------------*/
+
+#include "baci_artery_cvOneDBF_utils.h"
+
+#include <Epetra_Comm.h>
+
+BACI_NAMESPACE_OPEN
+
+namespace ARTCV::UTILS
+{
+  void executeSerial(const Epetra_Comm& comm, const std::function<void()>& code)
+  {
+    if (comm.MyPID() == 0)
+    {
+      code();
+    }
+  }
+}  // namespace ARTCV::UTILS
+
+BACI_NAMESPACE_CLOSE
